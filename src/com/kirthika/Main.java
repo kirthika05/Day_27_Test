8eef6716fcc59a84862dc549a520cf6efff32598
Kirthika S <kirthika05@gmail.com>
2017-01-27 23:37:30 -0600

    Initial commit

---
+++ src/com/kirthika/Main.java
@@ -0,0 +1,19 @@
+package com.kirthika;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println(5);
+        System.out.println("5 2");
+        System.out.println("0 -4 4 2 6");
+        System.out.println("4 2");
+        System.out.println("0 -1 4 2 ");
+        System.out.println("3 3");
+        System.out.println("0 0 4");
+        System.out.println("5 2");
+        System.out.println("0 -3 1 1 1");
+        System.out.println("2 3");
+        System.out.println("0 -3");
+    }
+}
